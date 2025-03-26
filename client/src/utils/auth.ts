e62e0cdf5608a4e4e8ed788c680249374c51e532
zomblic <zomblic@hotmail.com>
2025-03-26 08:56:19 -0400

    updated majority of the code

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,31 +1,48 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
+
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
-  isTokenExpired(token: string) {
+isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    const decodedToken: any = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decodedToken.exp < currentTime;
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
+
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
+
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
+
 
 export default new AuthService();
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,28 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const user = await User.findOne({
+    where: { username },
+  });
+
+  // If user is not found, send an authentication failed response
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  // Compare the provided password with the stored hashed password
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  // If password is invalid, send an authentication failed response
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  // Get the secret key from environment variables
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  // Generate a JWT token for the authenticated user
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.json({ token });  // Send the token as a JSON response
 };
 
 const router = Router();
