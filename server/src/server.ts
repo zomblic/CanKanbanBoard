70703276adde06e010446bee9d765b34dec0aab6
zomblic <189485834+zomblic@users.noreply.github.com>
2025-03-29 06:51:01 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(routes);
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
